ba8a1fa43713c1abf66fc00e2083f50d4c937d02
Mchrispin <157820510+Mchiir@users.noreply.github.com>
2024-08-27 22:19:13 +0200

    Update README.py

--- README.py
+++ README.py
@@ -6,9 +6,13 @@
 # while activated (venv), then bash: pip install Flask python-dotenv watchdog pymongo
 # Remember to create the requirements.txt(like package.json) bash: pip freeze > requirements.txt
 
-# After installations deactivate venv bash: deactivate
-# return to cmd
-# python server.py
+# Write your codes!
 
+
+# run the server.py
+  cmd:
+  python server.py
 
 # Happy hacking!
+
+
